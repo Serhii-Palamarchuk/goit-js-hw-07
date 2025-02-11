9f207b30d4ce31870af362bf7d8a7d526d23c081
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-02-11 03:07:59 +0200

    Add console logs for task-3, task-4, task-5, and task-6 initialization

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,7 @@
+'use strict';
+
+console.log("<=== TASK-5 ============================================>");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,7 @@
+'use strict';
+
+console.log("<=== TASK-6 ============================================>");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
