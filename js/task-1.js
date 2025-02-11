4875fc5df3945c1e2b245585e1e87d044002a965
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-02-11 02:58:38 +0200

    Add JavaScript functionality to log category details

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log("<=== TASK-1 ============================================>");
+
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach(category => {
+    const categoryTitle = category.querySelector('h2').textContent;
+    const categoryElementsCount = category.querySelectorAll('ul li').length;
+    
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryElementsCount}`);
+});
